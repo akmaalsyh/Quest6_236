9af9d439f3612608bd81c78ecf8a1bef4264eb14
akmaalsyh <taufansyah50@gmail.com>
2025-11-20 14:12:38 +0700

    Membuat SiswaViewModel dan fungsi setSiswa

---
+++ app/src/main/java/com/example/quest6_236/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.quest6_236.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(value = Siswa())
+
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls:MutableList<String>){
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                gender = ls[1],
+                alamat = ls[2])
+        }
+    }
+}
